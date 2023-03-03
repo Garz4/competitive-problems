31b93c8d3f9978cdf75ce5c69c1c3ff068023e4b
Garz4 <fancygarz4@gmail.com>
2023-03-02 23:21:44 -0600

    Follow conventions

--- euler/p2.cpp
+++ euler/p2.cpp
@@ -4,13 +4,13 @@
 
 #include "numbers.hpp"
 
-static constexpr unsigned long long LIMIT = 4000000;
+static constexpr unsigned long long target = 4000000;
 
 int main(void) {
   const auto fibonacci = get_fibonacci();
   unsigned long long current_sum = 0;
 
-  for (int i = 0; fibonacci[i] < LIMIT; ++i) {
+  for (int i = 0; fibonacci[i] < target; ++i) {
     if (fibonacci[i] % 2 == 0) {
       current_sum += fibonacci[i];
     }
