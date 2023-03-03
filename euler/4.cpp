721e32c4e9a90809015e3fedfa0af2628006a240
Garz4 <fancygarz4@gmail.com>
2023-03-02 23:55:48 -0600

    Add solution to 4

---
+++ euler/4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+// Both numbers must be 3-digit numbers.
+static constexpr int max_number = 999;
+static constexpr int initial_number = 100;
+
+static inline bool is_palindrome(const int& number) {
+  const std::string parse = std::to_string(number);
+
+  for (int i = 0, j = parse.length() - 1; i < j; ++i, --j) {
+    if (parse[i] != parse[j]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(void) {
+  int largest_palindrome = 0;
+  int current_number;
+
+  for (int i = initial_number; i <= max_number; ++i) {
+    for (int j = initial_number; j <= max_number; ++j) {
+      current_number = i * j;
+
+      if (is_palindrome(current_number)) {
+        largest_palindrome = std::max(largest_palindrome, current_number);
+      }
+    }
+  }
+
+  std::cout << largest_palindrome << std::endl;
+
+  return 0;
+}
